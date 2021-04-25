94a2dc0418db0b02d7dc7995072c619cb6e2f011
crazyfdf <343224608@qq.com>
2021-04-25 18:48:24 +0800

    no message

---
+++ part2/fed-e-task-02-02/notes/webpack/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "built.js",
+    path: path.resolve("dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
