72166bc4ddce467e6ac53c1b503835da73bab872
crazyfdf <343224608@qq.com>
2021-05-11 09:48:45 +0800

    03-03

---
+++ part3/fed-e-task-03-03/ssr/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class='index'>
+    Home
+  </view>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  async asyncData() {
+    const { data } = await axios({
+      method: "GET",
+      url: "/data.json",
+    });
+    return data;
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
