7de4c9fc4f4807b09a80ec4a4fa6a005d73da1ca
crazyfdf <343224608@qq.com>
2021-05-13 17:16:15 +0800

    修改03-03

--- part3/fed-e-task-03-04/blog-with-gridsome/gridsome.config.js
+++ part3/fed-e-task-03-04/blog-with-gridsome/gridsome.config.js
@@ -5,6 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "拉钩教育",
+  siteDescription: "大前端",
+  plugins: [],
+  templates: {},
+};
